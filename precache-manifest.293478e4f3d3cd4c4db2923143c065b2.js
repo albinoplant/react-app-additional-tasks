a9c4b62f446d533d574075c0140c2efba46b8243
albinoplant <mateuszmalecki@hotmail.com>
2019-06-21 21:33:31 +0200

    Updates

---
+++ precache-manifest.293478e4f3d3cd4c4db2923143c065b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57a0fca291c5c9b9b8f28e0cacc6f38",
+    "url": "/react-app-additional-tasks/index.html"
+  },
+  {
+    "revision": "3cd9f531715f56267b46",
+    "url": "/react-app-additional-tasks/static/css/main.3a3d05b4.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-app-additional-tasks/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "3cd9f531715f56267b46",
+    "url": "/react-app-additional-tasks/static/js/main.ce869c0d.chunk.js"
+  },
+  {
+    "revision": "8b0237ac1b884f64764b",
+    "url": "/react-app-additional-tasks/static/js/runtime~main.84f53d5a.js"
+  }
+]);
